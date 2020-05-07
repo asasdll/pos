ecb34f36dbb1e6f4d371b658e070d787ebdc578e
asasdll <lollipopwar-love@hotmail.co.th>
2020-05-07 13:31:29 +0700

    ryhrhe

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,231 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>Industry.INC | HTML Template</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Industry.INC HTML Template">
+	<meta name="keywords" content="industry, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section  -->
+	<header class="header-section clearfix">
+		<div class="site-navbar">
+			<!-- Logo -->
+			<a href="index.html" class="site-logo">
+				<img src="img/logo.png" alt="">
+			</a>
+			<div class="header-right">
+				<div class="header-info-box">
+					<div class="hib-icon">
+						<img src="img/icons/phone.png" alt="" class="">
+					</div>
+					<div class="hib-text">
+						<h6>+546 990221 123</h6>
+						<p>contact@industryalinc.com</p>
+					</div>
+				</div>
+			</div>
+			<!-- Menu -->
+			<nav class="site-nav-menu">
+				<ul>
+                    <li><a href="index.html">Home</a>
+                        <ul class="sub-menu">
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+						</ul>
+                    </li>
+                    <li><a href="about.html">About us</a>
+                        <ul class="sub-menu">
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                        </ul>
+                    </li>
+					<li><a href="solutions.html">Solutions</a>
+						<ul class="sub-menu">
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+						</ul>
+					</li>
+                    <li><a href="blog.html">Blog</a>
+                        <ul class="sub-menu">
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+						</ul>
+                    </li>
+                    <li><a href="contact.html">Contact</a>
+                        <ul class="sub-menu">
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+                            <li><a href="elements.html">Elements</a></li>
+						</ul>
+                    </li>
+				</ul>
+			</nav>
+
+		</div>
+	</header>
+    <!-- Header section end  -->
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+	
+	<!-- Hero section  -->
+		<div class="footer-buttom">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-4 order-2 order-lg-1 p-0">
+					<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+				</div>
+				<div class="col-lg-7 order-1 order-lg-2 p-0">
+					<ul class="footer-menu">
+						<li class="active"><a href="index.html">Home</a></li>
+						<li><a href="about.html">About us</a></li>
+						<li><a href="solutions.html">Solutions</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+
+	<!-- Search model -->
+	<div class="search-model">
+		<div class="h-100 d-flex align-items-center justify-content-center">
+			<div class="search-close-switch">+</div>
+			<form class="search-model-form">
+				<input type="text" id="search-input" placeholder="Search here.....">
+			</form>
+		</div>
+	</div>
+	<!-- Search model end -->
+	
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
